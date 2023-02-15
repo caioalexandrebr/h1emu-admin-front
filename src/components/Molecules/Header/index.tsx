7db363419a4dcdb3d0561a88ac8f5d762b1525d0
Caio Alexandre <caioalexandre.br@gmail.com>
2023-02-14 21:41:59 -0300

    first commit

---
+++ src/components/Molecules/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderStyles, Logo } from './styles';
+
+const Header = () => {
+  return (
+    <HeaderStyles>
+      <Logo src="./images/logo.png" />
+    </HeaderStyles>
+  );
+};
+
+export default Header;
---
+++ src/components/Molecules/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.header`
+  width: 100%;
+  height: 100px;
+  padding: 0 60px;
+  display: flex;
+  align-items: center;
+  background: #0e0e0e url(https://cdn.h1emu.com/patterns/3.png);
+`;
+
+export const Logo = styled.img`
+  width: 80px;
+`;
